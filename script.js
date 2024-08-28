de7c34ed80cbdeeae8eee235bf66e4aac87d2e59
rozhkovaei <116023705+rozhkovaei@users.noreply.github.com>
2024-08-28 23:50:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -17,35 +17,30 @@ let userid = document.createElement('p');
 userid.innerText = `${tg.initDataUnsafe.user.id}`;
 usercard.appendChild(userid);
 
-/*
+
 function HMAC_SHA256(key: string | Buffer, secret: string) {
-  return createHmac("sha256", key).update(secret);
+   return createHmac("sha256", key).update(secret);
 }
 
 function getCheckString(data: URLSearchParams) {
-	const items: [k: string, v: string][] = [];
+   const items: [k: string, v: string][] = [];
+   // remove hash
+   for (const [k, v] of data.entries()) if (k !== "hash") items.push([k, v]);
 
-	// remove hash
-	for (const [k, v] of data.entries()) if (k !== "hash") items.push([k, v]);
-
-	return items.sort(([a], [b]) => a.localeCompare(b)) // sort keys
-		.map(([k, v]) => `${k}=${v}`) // combine key-value pairs
-		.join("\n");
+   return items.sort(([a], [b]) => a.localeCompare(b)) // sort keys
+	.map(([k, v]) => `${k}=${v}`) // combine key-value pairs
+	.join("\n");
 }
-*/
 
 app.post("/validate-init", (req, res) => {
-	const data = new URLSearchParams(req.body);
+   const data = new URLSearchParams(req.body);
 
-//	const data_check_string = getCheckString(data);
-//	const secret_key = HMAC_SHA256("WebAppData", process.env.BOT_TOKEN!).digest();
-//	const hash = HMAC_SHA256(secret_key, data_check_string).digest("hex");
+   const data_check_string = getCheckString(data);
+   const secret_key = HMAC_SHA256("WebAppData", process.env.BOT_TOKEN!).digest();
+   const hash = HMAC_SHA256(secret_key, data_check_string).digest("hex");
 
-//	if (hash === data.get("hash"))
-		// validated!
-//		return res.json(Object.fromEntries(data.entries()));
+   if (hash === data.get("hash"))
+      return res.json(Object.fromEntries(data.entries()));
 
-//	return res.status(401).json({});
-
-return res.status(200).json({});
+   return res.status(401).json({});
 });
