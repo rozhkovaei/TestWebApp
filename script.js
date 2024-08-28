50b59f61c9b21eb17a27105bdaec422e316b27d3
rozhkovaei <116023705+rozhkovaei@users.noreply.github.com>
2024-08-28 23:47:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
-   let tg = window.Telegram.WebApp;
+import { createHmac } from "node:crypto";
 
-   tg.expand();
+let tg = window.Telegram.WebApp;
 
-   let usercard = document.getElementById("usercard");
+app.ready();
+tg.expand();
 
-   let profName = document.createElement('p');
-   profName.innerText = `${tg.initDataUnsafe.user.first_name}
-   ${tg.initDataUnsafe.user.last_name}
-   ${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
-   usercard.appendChild(profName);
+let usercard = document.getElementById("usercard");
 
-   let userid = document.createElement('p');
-   userid.innerText = `${tg.initDataUnsafe.user.id}`;
-   usercard.appendChild(userid);
+let profName = document.createElement('p');
+profName.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}
+${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
+usercard.appendChild(profName);
+
+let userid = document.createElement('p');
+userid.innerText = `${tg.initDataUnsafe.user.id}`;
+usercard.appendChild(userid);
+
+/*
+function HMAC_SHA256(key: string | Buffer, secret: string) {
+  return createHmac("sha256", key).update(secret);
+}
+
+function getCheckString(data: URLSearchParams) {
+	const items: [k: string, v: string][] = [];
+
+	// remove hash
+	for (const [k, v] of data.entries()) if (k !== "hash") items.push([k, v]);
+
+	return items.sort(([a], [b]) => a.localeCompare(b)) // sort keys
+		.map(([k, v]) => `${k}=${v}`) // combine key-value pairs
+		.join("\n");
+}
+*/
+
+app.post("/validate-init", (req, res) => {
+	const data = new URLSearchParams(req.body);
+
+//	const data_check_string = getCheckString(data);
+//	const secret_key = HMAC_SHA256("WebAppData", process.env.BOT_TOKEN!).digest();
+//	const hash = HMAC_SHA256(secret_key, data_check_string).digest("hex");
+
+//	if (hash === data.get("hash"))
+		// validated!
+//		return res.json(Object.fromEntries(data.entries()));
+
+//	return res.status(401).json({});
+
+return res.status(200).json({});
+});
