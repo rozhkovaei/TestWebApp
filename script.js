d6218d996d99310bfbd266f33372127ab1880ae4
rozhkovaei <116023705+rozhkovaei@users.noreply.github.com>
2024-08-28 23:39:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+   let tg = window.Telegram.WebApp;
 
+   tg.expand();
+
+   let usercard = document.getElementById("usercard");
+
+   let profName = document.createElement('p');
+   profName.innerText = `${tg.initDataUnsafe.user.first_name}
+   ${tg.initDataUnsafe.user.last_name}
+   ${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
+   usercard.appendChild(profName);
+
+   let userid = document.createElement('p');
+   userid.innerText = `${tg.initDataUnsafe.user.id}`;
+   usercard.appendChild(userid);
